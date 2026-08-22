942b06af2a6c60285e6c5a12c454b7538d7259d1
himanshukmwt <himanshukumawat8824@gmail.com>
2026-08-22 14:54:08 +0530

    function added to get a single note

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -40,3 +40,22 @@ export const getNotes = async (req, res) => {
         });
     }
 };
+
+export const getNote = async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id);
+
+        if (!note) {
+            return res.status(404).json({
+                message: "Note not found"
+            });
+        }
+
+        return res.status(200).json(note);
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
+};
